a81db23b901a8ac4befddff89bff26bb348f8d37
feign <2225744553@qq.com>
2026-07-13 18:24:02 +0800

    Initial commit

---
+++ Source/ImmortalPath/ImmortalPath.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ImmortalPath.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ImmortalPath, "ImmortalPath" );
